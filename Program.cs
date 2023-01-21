97230371c3a175b560d37ec71ca00c3f5f18f8ae
Akbermet Aseyinova <aseyinovva1212@gmail.com>
2023-01-21 21:38:02 +0300

    Init Project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using System.Runtime.InteropServices;
+
+namespace Test1
+{
+     class Program
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Set
+
+]
+
+
+        IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.dcb.kg/ru/");
+
+                
+        }
+    }
+}
